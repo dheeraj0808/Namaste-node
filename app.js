9b3840c8002baa09e8568821dbc675052adcc0b4
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-11 00:11:29 +0530

    concept added in app js for the calculator import and export

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ var b = 10;
 CalculateSum(a, b);
 substact(a, b);
 Multiply(a, b);
+// we can also use the functions from the Calculate module in the xyz.js file
+// because we have imported the Calculate module in the xyz.js file
+// we can also use the functions from the Calculate module in this file
+// because we have imported the Calculate module in this file
